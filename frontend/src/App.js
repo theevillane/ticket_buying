32983f15705dfa2a8fdc2c22a1afc71838f27d46
theevillane <brianochiengonyango2@gmail.com>
2024-12-09 13:04:57 +0300

    updates and first commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,16 +34,3 @@ function App() {
 }
 
 export default App;
-
-// frontend/src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementByI
-)
