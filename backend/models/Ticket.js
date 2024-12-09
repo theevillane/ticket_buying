414562f79a1361a09dee937e07b460dc53f29c97
theevillane <brianochiengonyango2@gmail.com>
2024-12-09 12:51:09 +0300

    first commit

---
+++ backend/models/Ticket.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const TicketSchema = new mongoose.Schema({
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+  purchaseDate: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: String,
+    enum: ['Purchased', 'Cancelled'],
+    default: 'Purchased'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Ticket', TicketSchema);
